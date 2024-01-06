0ee8dd57bc2951beb36a7d2a60e98a0145203a48
xsuler <aeonsule@outlook.com>
2024-01-06 11:15:10 +0800

    start

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  VITE_CORS_PROXY_URL: "CHANGEME",
+  ITE_CORS_PROXY_URL: "https://proxy.eon.ink",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZGI0ZTQ0YzExOGVhZTM0OWI5NmFhMTMwODdjYjEzMyIsInN1YiI6IjY1OThjMDQ2MGQxMWYyMDBlZWRjZjYyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tf2CyCi9LZxTUgI5pF5Fl8ESplHC4kqE3eymGrKqx9Q",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
